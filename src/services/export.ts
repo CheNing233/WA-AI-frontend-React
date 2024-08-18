9d803d8d33fa380b313cdd9075d22b43fb6425bd
ChenNing <1915664627@qq.com>
2024-08-19 03:44:39 +0800

    doc: 添加注释

--- src/services/export.ts
+++ src/services/export.ts
@@ -1,11 +1,13 @@
 import * as account from './modules/account'
 import * as sd from './modules/sd'
 import * as staticImage from './modules/staticImage'
+import * as dashboard from './modules/dashboard'
 
 const api = {
     account,
     sd,
-    staticImage
+    staticImage,
+    dashboard
 }
 
 export default api
