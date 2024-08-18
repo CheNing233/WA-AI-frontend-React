dea9d9b2566ec292368f3920cdae55a1ad86bf9d
ChenNing <1915664627@qq.com>
2024-08-19 03:34:37 +0800

    feat: 添加dashboard.ts

---
+++ src/services/modules/dashboard.ts
@@ -0,0 +1,31 @@
+import request from "@/services";
+import {AxiosResponse} from "axios";
+
+// 响应接口
+export interface GetCountRes {
+    /* */
+    success: boolean;
+
+    /* */
+    code: number;
+
+    /* */
+    errorMsg: string;
+
+    /* */
+    data: Record<string, unknown>;
+
+    /* */
+    total: number;
+}
+
+/**
+ * 每日数据
+ * @param {string} startDate
+ * @param {string} endDate
+ * @param {string} type
+ * @returns
+ */
+export function getCount(startDate: string, endDate: string, type: string): Promise<AxiosResponse<GetCountRes>> {
+    return request.get(`/dashboard?startDate=${startDate}&endDate=${endDate}&type=${type}`);
+}
